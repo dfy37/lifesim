daaff1297d01cff14a67f8b53af5c62659f43aa2
Ping Zhang <zhangping@infrawaves.com>
2026-02-02 08:58:55 +0000

    update

--- events/run.sh
+++ events/run.sh
@@ -1,20 +1,20 @@
-# python -m extraction \
-#   --input-root /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/data/twitter_archive_cleaned/twitter_archive_cleaned/shards \
-#   --output-root /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/data/events_labeled_data \
-#   --model /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/MODELS/Qwen3-32B \
-#   --base-url http://0.0.0.0:8000/v1 \
-#   --api-key "123" \
-#   --text-col tweet_text \
-#   --time-col created_at \
-#   --workers 16
-
-python -m check \
-  --input-root /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/data/events_labeled_data \
-  --output-root /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/data/events_labeled_data_checked \
-  --generator-model /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/MODELS/Qwen3-32B \
-  --validator-model /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/MODELS/Qwen3-32B \
+python -m extraction \
+  --input-root /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/data/twitter_archive_cleaned/twitter_archive_cleaned/shards \
+  --output-root /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/data/events_labeled_data \
+  --model /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/MODELS/Qwen3-32B \
   --base-url http://0.0.0.0:8000/v1 \
   --api-key "123" \
-  --epsilon 0.35 \
-  --max-rounds 3 \
-  --workers 16 \
+  --text-col tweet_text \
+  --time-col created_at \
+  --workers 16
+
+# python -m check \
+#   --input-root /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/data/events_labeled_data \
+#   --output-root /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/data/events_labeled_data_checked \
+#   --generator-model /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/MODELS/Qwen3-32B \
+#   --validator-model /inspire/hdd/project/socialsimulation/linjiayu-CZXS25120090/FYDUAN/MODELS/Qwen3-32B \
+#   --base-url http://0.0.0.0:8000/v1 \
+#   --api-key "123" \
+#   --epsilon 0.35 \
+#   --max-rounds 3 \
+#   --workers 16 \
