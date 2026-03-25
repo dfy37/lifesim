de5ec2f40ddbe1a3d6a8c8ea91817326d6584205
dfy37 <1356717485@qq.com>
2026-03-26 01:45:47 +0800

    update

--- demo/run.sh
+++
@@ -1,4 +0,0 @@
-python app.py \
-    --config "/Users/duanfeiyu/Documents/Fudan-DISC/Lifelong User Simulation/secrets/config.yaml" \
-    --events-path "/Users/duanfeiyu/Documents/Fudan-DISC/Lifelong User Simulation/lifesim/data/single_session/events.jsonl" \
-    --users-path "/Users/duanfeiyu/Documents/Fudan-DISC/Lifelong User Simulation/lifesim/data/single_session/users.jsonl" \
